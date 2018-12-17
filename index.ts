f1c709f28bd0afd524cf3248ecda862eedc36c85
Igor Zoriy <me@igorzoriy.com>
2018-12-17 16:57:29 +0100

    Add tslint

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import * as Parser from "rss-parser"
 import { TelegramClient } from "messaging-api-telegram"
+import * as Parser from "rss-parser"
 
 const rssUri = "https://habr.com/rss/feed/posts/all/5cdefeee2ea0b5687dcfae0cbf44f7e3/?hl=ru&fl=ru"
 const telegramAccessToken = "739015655:AAFGxGYxrkYl8F69B1ItTaWyZFZJU5o9WqA"
@@ -8,10 +8,8 @@ const channelId = "-1001166301659"
 const parser = new Parser()
 const client = TelegramClient.connect(telegramAccessToken)
 
-parser.parseURL(rssUri).then(feed => {
-    feed.items.forEach(item => {
+parser.parseURL(rssUri).then((feed) => {
+    feed.items.forEach((item) => {
         client.sendMessage(channelId, item.title)
     })
 })
-
-
