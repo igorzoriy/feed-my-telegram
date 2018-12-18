49211c858766985c2e9057e286c3c2fe0f2c35a7
Igor Zoriy <me@igorzoriy.com>
2018-12-18 12:13:14 +0100

    Move sources to json file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-import { load } from "dotenv-extended"
+import * as dotenv from "dotenv-extended"
 import { TelegramClient } from "messaging-api-telegram"
+import * as sources from "../sources.json"
 import { RssFeeder } from "./RssFeeder"
 
-load()
-
-const rssUri = "https://habr.com/rss/feed/posts/all/5cdefeee2ea0b5687dcfae0cbf44f7e3/?hl=ru&fl=ru"
-const channelId = "-1001166301659"
-
+dotenv.load()
 const client = TelegramClient.connect(process.env.TELEGRAM_ACCESS_TOKEN)
-
-const feeder = new RssFeeder({
-    channelId,
-    client,
-    uri: rssUri,
+sources.forEach((item) => {
+    const feeder = new RssFeeder({
+        channelId: item.channelId,
+        client,
+        uri: item.uri,
+    })
+    feeder.start()
 })
-feeder.start()
