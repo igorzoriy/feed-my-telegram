a0ca45fe73da90948d764dcefb92d17c32117b3b
Igor Zoriy <me@igorzoriy.com>
2019-01-06 23:17:50 +0100

    Add error handling

--- src/RssFeeder.ts
+++ src/RssFeeder.ts
@@ -1,10 +1,18 @@
 import * as Keyv from "keyv"
-import { TelegramClient } from "messaging-api-telegram"
+import { SendMessageResponse, TelegramClient } from "messaging-api-telegram"
 import * as Parser from "rss-parser"
+import { Logger } from "winston"
+import { sleep } from "./utils"
+
+interface IRssItem {
+    title: string
+    guid: string
+}
 
 export class RssFeeder {
     private channelId: string
     private client: TelegramClient
+    private logger: Logger
     private parser
     private storage: Keyv
     private timerId: NodeJS.Timeout
@@ -14,39 +22,72 @@ export class RssFeeder {
         uri,
         client,
         channelId,
+        logger,
         storage,
     }: {
         uri: string,
         client: TelegramClient,
         channelId: string,
+        logger: Logger,
         storage: Keyv,
     }) {
         this.uri = uri
         this.client = client
         this.channelId = channelId
+        this.logger = logger
         this.storage = storage
-
         this.parser = new Parser()
     }
 
-    public start() {
-        this.timerId = setInterval(this.tick.bind(this), 500)
+    public async start() {
+        this.timerId = setInterval(this.tick.bind(this), 1000)
     }
 
     public stop() {
         clearInterval(this.timerId)
     }
 
-    private tick() {
-        this.parser.parseURL(this.uri).then(({ items }) => {
-            items.reverse().forEach(async (item) => {
+    private logError(message: string) {
+        this.logger.error(`RssFeeder - ${message}`)
+    }
+
+    private async tick() {
+        let items: IRssItem[]
+        try {
+            const feed = await this.parser.parseURL(this.uri)
+            items = feed.items
+        } catch (ex) {
+            this.logError(`${ex} - ${this.uri}`)
+            this.stop()
+            await sleep(60000)
+            this.start()
+            return
+        }
+
+        items.reverse().forEach(async (item) => {
+            try {
                 if (await this.storage.get(item.guid)) {
                     return
                 }
+            } catch (ex) {
+                this.logError(ex)
+                return
+            }
 
-                await this.client.sendMessage(this.channelId, item.title)
+            let result: SendMessageResponse
+            try {
+                result = await this.client.sendMessage(this.channelId, item.title)
+            } catch (ex) {
+                this.logError(ex)
+                return
+            }
+
+            try {
                 await this.storage.set(item.guid, Date.now())
-            })
+            } catch (ex) {
+                this.client.deleteMessage(this.channelId, result.message_id)
+                this.logError(ex)
+            }
         })
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
 import * as dotenv from "dotenv-extended"
 import * as Keyv from "keyv"
 import { TelegramClient } from "messaging-api-telegram"
+import { createLogger, format, transports } from "winston"
 import * as sources from "../sources.json"
 import { RssFeeder } from "./RssFeeder"
 
 dotenv.load()
 
+const { combine, printf, timestamp, colorize } = format
+const pretty = printf((data) => `${data.timestamp} ${data.level}: ${data.message}`)
+const logger = createLogger({
+    format: combine(
+        timestamp(),
+        colorize({ all: true }),
+        pretty,
+    ),
+    transports: [
+        new transports.Console(),
+    ],
+})
+
 const client = TelegramClient.connect(process.env.TELEGRAM_ACCESS_TOKEN)
 const storage = new Keyv("sqlite://./storage.sqlite")
-storage.on("error", (err) => console.error(err))
+storage.on("error", (err) => logger.error(err))
 
 sources.forEach((item) => {
     const feeder = new RssFeeder({
         channelId: item.channelId,
         client,
+        logger,
         storage,
         uri: item.uri,
     })
