fd7c28c7fd53021af992052536c2ccac1c66915d
Igor Zoriy <me@igorzoriy.com>
2019-01-10 13:46:25 +0100

    Fix getShortUrl function

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import * as got from "got"
 
 export const getShortLink = async (link: string) => {
     const { body } = await got(`https://clck.ru/--?url=${link}`)
-    return body
+    return body.length <= 30 ? body : link
 }
 
 export const getJsonFromUrl = async (url: string) => {
